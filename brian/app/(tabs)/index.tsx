de53075be13b41b73ab0504b39e862265fa8cdae
Cheberlink <briancheberle@gmail.com>
2025-05-21 12:38:22 -0300

    Sim

---
+++ brian/app/(tabs)/index.tsx
@@ -0,0 +1,24 @@
+import { Text } from "react-native";
+import { View } from "react-native-reanimated/lib/typescript/Animated";
+
+export default function Index() {
+  const nome = "Brian";
+
+  return(
+    <View>
+      <Text>Jogo da Velha</Text>
+      <Text>Vencedor</Text>
+
+        <View>
+          <View></View>
+          <View></View>
+          <View></View>
+        </View>
+
+    </View>
+  )
+
+
+
+}
+
